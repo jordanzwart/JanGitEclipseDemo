9325a7d3a58cf05c263ac1e004aad371eaf4af91
jordanzwart <ZWART.JORDAN20@GMAIL.COM>
2018-01-23 14:05:48 -0500

    first commit

---
+++ src/GitHelloWorld.java
@@ -0,0 +1,12 @@
+/*
+ * Jordan Zwart
+ * This is a simple hello program that will be used to push to GitHub
+ */
+
+public class GitHelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
